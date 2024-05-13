4e5ab994145e49d58dfee123c30fbb8400f269d9
sheli <sheli@10.48.16.131>
2024-05-13 14:00:11 -0400

    Initial commit

---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author sheli
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Student[] studentList = new Student[3];
+        Student s1 = new Student();
+        s1.setName("Sheliey Master");
+        Student s2 = new Student();
+        s2.setName("Palak Shah");
+        Student s3 = new Student();
+        s3.setName("Sneha Christian");
+        studentList[0]=s1;
+        studentList[1]=s2;
+        studentList[2]=s3;
+        
+        for (Student s : studentList){
+            System.out.println(s.getName());
+        }
+        
+        
+    }
+    
+}
